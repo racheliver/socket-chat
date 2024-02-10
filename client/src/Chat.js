3ae450819d705f7c59a6b73371757db0858d1df7
RACHELI VERECHZON <racheliver@gmail.com>
2024-02-10 16:58:16 +0200

    changes css

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -9,51 +9,73 @@ import Avatar from "@mui/material/Avatar";
 import ScrollToBottom from "react-scroll-to-bottom";
 
 function Chat({ socket, username, room }) {
-    const [currentMessage, setCurrentMessage] = useState("");
-    const [messageList, setMessageList] = useState([]);
-  
-    const sendMessageHandler = async () => {
-      if (currentMessage !== "") {
-        const messageData = {
-          room: room,
-          author: username,
-          message: currentMessage,
-          time:
-            new Date(Date.now()).getHours() +
-            ":" +
-            new Date(Date.now()).getMinutes(),
-        };
-  
-        await socket.emit("send_message", messageData);
-        setMessageList((list) => [...list, messageData]);
-        setCurrentMessage("");
-      }
-    };
-  
-    useEffect(() => {
-      socket.on("receive_message", (data) => {
-        setMessageList((list) => [...list, data]);
-      });
-    }, [socket]);
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  const sendMessageHandler = async () => {
+    if (currentMessage !== "") {
+      const messageData = {
+        room: room,
+        author: username,
+        message: currentMessage,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+
+      await socket.emit("send_message", messageData);
+      setMessageList((list) => [...list, messageData]);
+      setCurrentMessage("");
+    }
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageList((list) => [...list, data]);
+    });
+  }, [socket]);
   return (
-    <Paper style={{ display: 'flex', flexDirection: 'column', height: '100%' }} elevation={3}>
-      <div style={{ display: 'flex', alignItems: 'center', padding: '8px', backgroundColor: '#3f51b5', color: '#fff' }} className="chat-header">
+    <Paper
+      style={{ display: "flex", flexDirection: "column", height: "100%" }}
+      elevation={3}
+    >
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          padding: "8px",
+          backgroundColor: "#3f51b5",
+          color: "#fff",
+        }}
+        className="chat-header"
+      >
         <Typography variant="h6">Chat App</Typography>
       </div>
       <div className="chat-body">
-        <ScrollToBottom className="message-container">
-          {messageList.map((messageContent,index) => {
+        <ScrollToBottom className="message-container msgs">
+          {messageList.map((messageContent, index) => {
             return (
               <div
                 key={index}
-                className="message"
-                id={username === messageContent.author ? "you" : "other"}
+                className={`msg ${
+                  username === messageContent.author
+                } ? sent : recevied`}
               >
-                <div>
-                  <div className="message-content">
-                    <p>{messageContent.message} = {messageContent.time}{messageContent.author}</p>
-                  </div>
-                </div>
+                  <p>
+                    {messageContent.message} = {messageContent.time}
+                    {messageContent.author}
+                  </p>
+                  <Avatar
+              variant="dot"
+              sx={{
+                bgcolor: `deepOrange[${
+                  100 * messageContent.author.length
+                }]`,
+              }}
+            >
+              {messageContent.author.slice(1)}
+            </Avatar>
               </div>
             );
           })}
@@ -72,6 +94,6 @@ function Chat({ socket, username, room }) {
       </div>
     </Paper>
   );
-};
+}
 
 export default Chat;
